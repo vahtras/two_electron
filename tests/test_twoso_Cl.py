ff4c55bd17ea5f72a9371483ce58fbfac18f50f4
Olav Vahtras <vahtras@kth.se>
2016-05-31 19:14:13 +0200

    Fix density generation in test

--- tests/test_twoso_Cl.py
+++ tests/test_twoso_Cl.py
@@ -14,22 +14,27 @@ class TestSpinOrbitCl(unittest.TestCase):
         self.ao2soint = os.path.join(suppdir, "AO2SOINT")
         self.cmo = np.loadtxt(os.path.join(suppdir, "cmo")).view(matrix)
 
-        norb = [9, 4, 4, 2, 4, 2, 2, 0]
-        self.iorb = [sum(norb[:i]) for i in range(8)]
-        self.occa = [3, 2, 2, 0, 2, 0, 0, 0]
-        self.occb = [3, 1, 2, 0, 2, 0, 0, 0]
+        norb = np.array([9, 4, 4, 2, 4, 2, 2, 0])
+        self.iorb = np.array([sum(norb[:i]) for i in range(8)])
+        self.occa = np.array([3, 2, 2, 0, 2, 0, 0, 0])
+        self.occb = np.array([3, 1, 2, 0, 2, 0, 0, 0])
 
         nbast = sum(norb)
         self.nbast = nbast
-        self.da = self.get_dens(self.occa)
-        self.db = self.get_dens(self.occb)
+        self.da = self.get_dens(self.occa, self.iorb)
+        self.db = self.get_dens(self.occb, self.iorb)
 
-    def get_dens(self, ndim):
+    def get_dens(self, ndim, idim, nocc=None):
+        if nocc is None:
+            nocc_ = [1]*len(ndim)
+        else:
+            nocc_ = nocc
+
         dens = matrix((self.nbast, self.nbast))
-        for ni, offset in zip(ndim, self.iorb):
+        for ni, no, offset in zip(ndim, nocc_, idim):
             if ni > 0:
                 cmoi = self.cmo[:, offset: offset + ni]
-                dens += cmoi*cmoi.T
+                dens += no*cmoi*cmoi.T
 
         return dens
 
@@ -40,10 +45,25 @@ class TestSpinOrbitCla(TestSpinOrbitCl):
     def setUp(self):
         TestSpinOrbitCl.setUp(self)
 
+        nish = np.array([3, 1, 1, 0, 1, 0, 0, 0])
+        nash = np.array([0, 1, 1, 0, 1, 0, 0, 0])
+        self.di = 2*self.get_dens(nish, self.iorb)
+        self.dv = self.get_dens(
+            nash,
+            self.iorb + nish,
+            nocc=[0, 1, 2, 0, 2, 0, 0, 0]
+            )
+
+    def test_internal(self):
         nish = [3, 1, 1, 0, 1, 0, 0, 0]
-        nash = [0, 1, 1, 0, 1, 0, 0, 0]
-        self.di = 2*self.get_dens(nish)
-        self.dv = 2*self.get_dens(nash)
+        nocc = [2, 2, 2, 0, 2, 0, 0, 0]
+        np.testing.assert_allclose(
+            2*self.get_dens(nish, self.iorb), 
+            self.get_dens(nish, self.iorb, nocc)
+            )
+
+    def test_internal_densities(self):
+        np.testing.assert_allclose(self.da+self.db, self.di+self.dv)
 
 
     def test_fc(self):
@@ -51,11 +71,12 @@ class TestSpinOrbitCla(TestSpinOrbitCl):
         fcmo = self.cmo.T*fc*self.cmo
         np.testing.assert_almost_equal(fcmo[10, 14], -20.80725730)
 
-    @unittest.skip('holdon')
+    @unittest.skip('wait')
     def test_fab(self):
         fa, fb = twoso.fockab(self.da, self.db, 'z', filename=self.ao2soint)
-        fcmo = self.cmo.T*(fa-fb)*self.cmo
-        np.testing.assert_almost_equal(fcmo[10, 14], -20.80725730)
+        fc = twoso.fock(self.di, 'z', filename=self.ao2soint)
+        fv = twoso.fock(self.dv, 'z', filename=self.ao2soint)
+        np.testing.assert_almost_equal(0.5*(fa - fb), fc + fv)
 
 
 class TestSpinOrbitClb(TestSpinOrbitCl):
@@ -65,7 +86,7 @@ class TestSpinOrbitClb(TestSpinOrbitCl):
 
         nish = [3, 1, 1, 0, 2, 0, 0, 0]
         nash = [0, 1, 1, 0, 0, 0, 0, 0]
-        self.di = 2*self.get_dens(nish)
+        self.di = 2*self.get_dens(nish, self.iorb)
 
     def test_fc(self):
         fc = twoso.fock(self.di, 'z' , filename=self.ao2soint)
